4ba9ea8728ece8e7ff8b942b61f28959a3d0f39f
Mark Fontenot <markefontenot@gmail.com>
2018-05-29 09:59:13 +0200

    added the stubbed file io sample functions to main.cpp

--- FileIO/main.cpp
+++ FileIO/main.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
+
+void sampleWritingToFile();
+void sampleReadingFromFile();
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+
     return 0;
+}
+
+
+void sampleWritingToFile ()
+{
+
+//    Step 1: Create a file streaming object
+
+
+//    Step 2: Connect the file streaming object to a file
+
+
+//    Step 3: Check to make sure the connection was successful
+
+
+//    Step 4: Write output to the file (just like as if it were cout)
+
+
+//    Step 5: Close the file.
+
+
+}
+
+/**
+ * precondition: a file must exist in the build directory that you're
+ * going to open and read from.  It should contain 10 integers, each on a
+ * separate line.
+ */
+
+void sampleReadingFromFile()
+{
+
+//    Step 1: Create a file streaming object for input
+
+
+//    Step 2: Connect the file streaming object to a file
+
+
+//    Step 3: Make sure the connection was successful
+
+
+//    Step 4: Read from the file
+
+
+//    Step 5: Close the file.
+
+
 }
